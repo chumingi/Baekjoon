b1295936cce27486de23d0f3ea8502357f3a9848
Mingi Chu <stressb00m@gachon.ac.kr>
2025-05-05 12:25:37 +0900

    Automation

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+python organize_boj.py
+python generate_readme.py
+pause
--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++
@@ -1,6 +0,0 @@
-import sys
-input = sys.stdin.readline
-N, x = map(int, input().split())
-arr = list(map(int, input().split()))
-for i in range(N):
-    if arr[i] < x: print(arr[i], end=" ")
