e9f8b1885c93e66083b9f0f371f042c8a988805f
추민기 <chumingi794@gmail.com>
2025-05-06 18:26:55 +0900

    [Bronze III] Title: 홀수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2576. 홀수/홀수.py
+++ 백준/Bronze/2576. 홀수/홀수.py
@@ -1,10 +1,8 @@
-nums = []
-for _ in range(7):
-    num = int(input())
-    if (num%2 == 1):
-        nums.append(num)
-if len(nums) == 0:
-    print(-1)
+nums = [int(input()) for _ in range(7)]
+odds = [n for n in nums if n % 2 == 1]
+
+if odds:
+    print(sum(odds))
+    print(min(odds))
 else:
-    print(sum(nums))
-    print(min(nums))
+    print(-1)
