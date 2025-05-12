28ee0313af729047db6721758bfc9334036ede68
추민기 <chumingi794@gmail.com>
2025-05-12 09:04:38 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
@@ -1,9 +1,3 @@
-import sys
-input = sys.stdin.readline
-write = sys.stdout.write
-
 N = int(input())
-for i in range(N):
-    for j in range(i+1):
-        write("*")
-    write("\n")
+for i in range(1, N+1):
+    print('*'*i)
