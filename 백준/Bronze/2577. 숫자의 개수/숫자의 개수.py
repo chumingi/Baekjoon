671e08605957f231b5a58922256fabd6495ec216
추민기 <chumingi794@gmail.com>
2025-05-20 22:48:27 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,10 +1,10 @@
 A = int(input())
 B = int(input())
 C = int(input())
-abc = str(A * B * C)
-ztn = [0] * 10
+product = str(A * B * C)
+digit_count = [0] * 10
 
-for c in abc:
-    ztn[int(c)] += 1
-for i in range(10):
-    print(ztn[i])
+for digit in product:
+    digit_count[int(digit)] += 1
+for count in digit_count:
+    print(count)
