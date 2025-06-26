88626536af54169e7dba23e309e717156d79f766
추민기 <chumingi794@gmail.com>
2025-06-27 00:33:49 +0900

    [Bronze II] Title: Strfry, Time: 108 ms, Memory: 34900 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.py
+++ 백준/Bronze/11328. Strfry/Strfry.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import sys
 input = sys.stdin.readline
 write = sys.stdout.write
@@ -5,4 +6,4 @@ write = sys.stdout.write
 N = int(input())
 for i in range(N):
     first, second = input().split()
-    write("Possible\n") if sorted(first) == sorted(second) else write("Impossible\n")
+    write("Possible\n") if Counter(first) == Counter(second) else write("Impossible\n")
