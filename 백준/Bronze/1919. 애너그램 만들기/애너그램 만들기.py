0fdf150e8d9198479073ac540bbdb87e6e054d43
추민기 <chumingi794@gmail.com>
2025-06-27 18:59:07 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 56 ms, Memory: 34876 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.py
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.py
@@ -1,8 +1,9 @@
 from collections import Counter
-c = Counter
 
 first = input()
 second = input()
-c1 = c(first) - c(second)
-c2 = c(second) - c(first)
-print(c1.total() + c2.total())
+
+diff = Counter(first) - Counter(second)
+diff += Counter(second) - Counter(first)
+
+print(sum(diff.values()))
